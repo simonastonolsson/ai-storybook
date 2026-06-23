54c23949249a257dbda4354c5ffc2d2a853c427e
simonastonolsson <simon.olsson.sol@gmail.com>
2026-06-23 09:57:25 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
-<>
+export default function Page() {
+  return (<>
 <>
 <div className="mt-12 w-full max-w-2xl">
           <div className="relative rounded-[2rem] border-4 border-dashed border-purple-300/70 bg-white/80 p-6 shadow-xl backdrop-blur sm:p-8">
@@ -23,3 +24,6 @@
             </button>
           </div>
         </div>
+
+  )
+}
